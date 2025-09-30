b0114b54f5563f1c4ec4aec49ede7694ffb49cb6
oleksandrmedytskyi <omedytskyi@prosperityehr.com>
2025-09-30 23:51:26 +0200

    Setup Container to use Marketing.

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -7,23 +7,22 @@ const packagingJson = require('../package.json');
 const devConfig = {
   mode: 'development',
   devServer: {
-    port: 8081,
+    port: 8080,
     historyApiFallback: {
       index: 'index.html',
     },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+      },
+      shared: packagingJson.dependencies,
+    }),
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
-    new ModuleFederationPlugin({
-      name: 'marketing',
-      filename: 'remoteEntry.js',
-      exposes: {
-        './MarketingApp': './src/main',
-      },
-      shared: packagingJson.dependencies,
-    })
   ],
 };
 
